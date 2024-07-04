1071db533e479591924d79f643fbd0220e036344
Pavan Kalyan <127199710+PavanUmmagani07@users.noreply.github.com>
2024-07-04 22:06:56 +0530

    Add files via upload

---
+++ ContactUs.js
@@ -0,0 +1,13 @@
+firstNameEl = document.getElementById('firstName');
+lastNameEl = document.getElementById('lastName');
+emailEl = document.getElementById('email');
+mobileNumberEl = document.getElementById('mobileNumber');
+commentsEl = document.getElementById('comments');
+submitBtnEl = document.getElementById('submitBtn');
+
+
+/*submitBtnEl.addEventListener('submit',submitButton());
+function submitButton(){
+     console.log("Submitted")
+}
+*/
